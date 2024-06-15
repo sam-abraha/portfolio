691c75f7f4250c53d263aa4cb9bb0b573644b49e
sam <samuelabraha@gmail.com>
2024-06-15 23:36:14 +0200

    Modify Vite Configs to include video folder in build output

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'videos/*',
+          dest: 'videos'
+        }
+      ]
+    })
+  ],
   build: {
     outDir: 'dist',
   },
