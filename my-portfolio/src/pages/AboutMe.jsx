6609ba6f9cb0e947b590810163b5e6f6db5980d1
sam <samuelabraha@gmail.com>
2024-07-19 16:40:39 +0200

    feat(components): update design with better typography and animations to showcase skills section
    Added a endless sliding loop for my tech stack

--- my-portfolio/src/pages/AboutMe.jsx
+++ my-portfolio/src/pages/AboutMe.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { FaJava, FaJs, FaPython, FaGitAlt, FaReact, FaNode, FaFileExcel, FaFilePowerpoint, FaGit } from 'react-icons/fa';
-import { DiPostgresql, DiMongodb } from 'react-icons/di';
+import { FaJava, FaGitAlt, FaReact, FaNode } from 'react-icons/fa';
+import { DiPostgresql } from 'react-icons/di';
+import { SiTypescript, SiMicrosoftexcel, SiMicrosoftpowerpoint, SiPostman, SiPostgresql} from 'react-icons/si'
+import { motion } from 'framer-motion';
 
 const TailwindIcon = () => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -8,61 +10,87 @@ const TailwindIcon = () => (
   </svg>
 );
 
+const icons = [
+  <FaJava className="text-6xl text-blue-700" />,
+  <SiTypescript className="text-6xl text-blue-700" />,
+  <FaReact className="text-6xl text-blue-500" />,
+  <FaNode className="text-6xl text-green-500" />,
+  <TailwindIcon className="text-6xl" />,
+  <FaGitAlt className="text-6xl text-orange-500" />,
+  <DiPostgresql className="text-6xl text-blue-800" />,
+];
+
 export default function AboutMe() {
   return (
-    <div className="container mx-auto mt-20 p-4">
-      <div className="max-w-3xl mx-auto p-8 border border-transparent rounded-lg shadow-xl mb-8 bg-gradient-to-r from-gray-700 via-gray-900 to-black text-white">
-        <h1 className="text-4xl font-extrabold mb-6 text-center">About Me</h1>
-        <p className="text-lg leading-relaxed">
-          I am a computer science student based near Frankfurt, passionate about solving problems and constantly seeking new knowledge,
-          particularly in software engineering, software design and cloud. My goal is to enhance my skills and broaden my knowledge in these areas.
-          When I'm not coding, I enjoy playing basketball and playing musical instruments.
-        </p>
-      </div>
-      <div className="max-w-3xl mx-auto p-8 border border-transparent rounded-lg text-gray-700 shadow-xl mb-8 bg-white">
-        <h1 className="text-3xl font-bold mb-6 text-gray-900">Skills</h1>
-        <ul className="list-disc pl-5 space-y-4">
-          <li className="text-lg flex justify-between items-center">
-            <span className="font-semibold">Programming languages: Java, JavaScript, Python</span>
-            <span className="flex items-center">
-              <FaJava className="ml-2 text-blue-700" />
-              <FaJs className="ml-2 text-yellow-300" />
-              <FaPython className="ml-2 text-blue-500" />
-            </span>
-          </li>
-          <li className="text-lg flex justify-between items-center">
-            <span className="font-semibold">Frameworks / Libraries: React, Express</span>
-            <span className="flex items-center">
-              <FaReact className="ml-2 text-blue-500" />
-              <FaNode className="ml-2 text-green-500" />
-              <TailwindIcon className="ml-2" />
-            </span>
-          </li>
-          <li className="text-lg flex justify-between items-center">
-            <span className="font-semibold">Tools: Git, Excel, PowerPoint</span>
-            <span className="flex items-center">
-              <FaGitAlt className="ml-2 text-orange-500" />
-              <FaFileExcel className="ml-2 text-green-600" />
-              <FaFilePowerpoint className="ml-2 text-red-600" />
-            </span>
-          </li>
-          <li className="text-lg flex justify-between items-center">
-            <span className="font-semibold">Databases: MongoDB, PostgreSQL</span>
-            <span className="flex items-center">
-              <DiMongodb className="ml-2 text-green-700" />
-              <DiPostgresql className="ml-2 text-blue-800" />
-            </span>
-          </li>
-          <li className="text-lg flex justify-between items-center">
-            <span className="font-semibold">Languages: German, English, Tigrinya</span>
-          </li>
-        </ul>
-      </div>
-      <div className="max-w-3xl mx-auto p-8 border border-transparent rounded-lg text-gray-700 shadow-xl bg-white">
-        <h1 className="text-3xl font-bold mb-6 text-gray-900">Education</h1>
-        <ul className="list-disc pl-5 space-y-4">
-          <li className="text-lg">Computer Science, Frankfurt University of Applied Science</li>
-        </ul>
+    <div className="bg-primary min-h-full flex flex-col items-center mt-10 py-16">
+      <div className="prose lg:prose-gray mx-auto p-4 text-gray-700">
+        <div className="mb-8">
+          <h1 className="text-4xl font-bold mb-6 bg-gradient-to-r py-2 from-pink-500 via-yellow-500 to-red-500 bg-clip-text text-transparent">Hi again, I'm Samuel.</h1>
+          <p className="text-lg leading-relaxed text-justify">
+            I am a computer science student based near Frankfurt, excited about solving problems and constantly seeking new knowledge,
+            particularly in software engineering, software design and cloud. My goal is to enhance my skills and broaden my knowledge in these areas.
+            When I'm not coding, I enjoy playing basketball and playing musical instruments passionately.
+          </p>
+        </div>
+        <div className="mb-8">
+          <h1 className="text-xl font-bold mb-6 text-gray-900">Skills</h1>
+          <ul className="list-disc pl-5 space-y-4">
+            <li className="text-lg flex justify-between items-center">
+              <span className="font-semibold">Programming languages: Java, Type/JavaScript</span>
+              <span className="flex items-center">
+                <FaJava className="ml-2 text-orange-700" />
+                <SiTypescript className="ml-2 text-blue-700" />
+              </span>
+            </li>
+            <li className="flex justify-between items-center text-lg">
+              <span className="font-semibold">Frameworks: React, Express</span>
+              <span className="flex items-center">
+                <FaReact className="ml-auto text-blue-500" />
+                <FaNode className="ml-2 text-green-500" />
+                <TailwindIcon className="ml-2" />
+              </span>
+            </li>
+            <li className="flex justify-between items-center text-lg">
+              <span className="font-semibold">Tools: Git, Postman, Excel, PowerPoint</span>
+              <span className="flex items-center">
+                <FaGitAlt className="ml-2 text-orange-500" />
+                <SiPostman className="ml-2 text-orange-500" />
+                <SiMicrosoftexcel className="ml-2 text-green-600" />
+                <SiMicrosoftpowerpoint className="ml-2 text-red-600" />
+              </span>
+            </li>
+            <li className="flex justify-between items-center text-lg">
+              <span className="font-semibold">Databases: PostgreSQL</span>
+              <span className="flex items-center">
+                <SiPostgresql className="ml-2 text-blue-800" />
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div className="relative overflow-hidden mt-6 h-20">
+            <motion.div
+              className="flex space-x-6"
+              animate={{ x: ['0%', '-100%'] }}
+              transition={{ repeat: Infinity, duration: 20, ease: 'linear' }}
+            >
+              {icons.map((icon, index) => (
+                <div key={index} className="flex-none">
+                  {icon}
+                </div>
+              ))}
+              {icons.map((icon, index) => (
+                <div key={index + icons.length} className="flex-none">
+                  {icon}
+                </div>
+              ))}
+            </motion.div>
+          </div>
+        <div>
+          <h1 className="text-xl font-bold mb-6 text-gray-900">Education</h1>
+          <ul className="list-disc pl-5 space-y-4">
+            <li className="text-lg">Computer Science, Frankfurt University of Applied Science</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
