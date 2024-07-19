657a4e390eba49f3b287a1ba2d31da3e7da671ec
sam <samuelabraha@gmail.com>
2024-07-19 16:22:35 +0200

    feat : update Footer design
    Added logos for networking in Footer
    
    Change background color for a minimalistic look
    
    Updated the width and height of Footer

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -1,22 +1,21 @@
 import { Link } from "react-router-dom"
+import {FaGithub} from 'react-icons/fa'
+import { MdEmail } from 'react-icons/md'
 
 export default function Footer() {
     return(
-        <div className="bottom-0 right-0 left-0 z-50 bg-black text-stone text-sm p-4">
+        <div className="bottom-0 right-0 left-0 z-50 text-dimgrey text-sm p-16 font-sans bg-secondary">
         <footer>
-          <div className="flex flex-col md:flex-row justify-evenly">
-            <div className="mb-4 md:mb-0">
-              <p className="font-semibold">Contacts</p>
-              <ul>
-                <li>
-                  <Link to={"https://github.com/sam-abraha"} className="hover:underline">Github</Link>
-                </li>
-                <li>
-                  <Link to={"mailto:samuelabrh@gmail.com"} className="hover:underline">Email</Link>
-                </li>
-              </ul>
+          <div>
+            <div className="mb-4 md:mb-0 flex justify-center space-x-10 text-4xl">
+              <Link to={"mailto:samuelabrh@gmail.com"}>
+                    <MdEmail />
+              </Link>
+              <Link to={"http://github.com/sam-abraha"}>
+                    <FaGithub />
+              </Link>
             </div>
-            <div className="mb-4 md:mb-0">
+            <div className="mb-4 md:mb-0 flex flex-col items-center mt-8">
               <p className="font-bold">Samuel Abraha</p>
               <p className="text-xs">Built with ReactJS.</p>
             </div>
