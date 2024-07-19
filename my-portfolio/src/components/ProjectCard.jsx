5bad299a8cdc060eac21d21dbe2a3ff018031079
sam <samuelabraha@gmail.com>
2024-07-19 16:25:28 +0200

    feat(components): update ProjectCards design
    Updated the width and height as well as the background color

--- my-portfolio/src/components/ProjectCard.jsx
+++ my-portfolio/src/components/ProjectCard.jsx
@@ -13,8 +13,8 @@ export default function ProjectCard({ videoSrc, techstack, title, description, i
   }, []);
 
   return (
-    <div className="flex flex-col md:flex-row w-full max-w-6xl p-5 my-10 bg-transparent shadow-lg rounded-xl transition-transform transform hover:scale-105 overflow-x-auto">
-      <div className="flex-1 flex-shrink-0 flex flex-col items-start md:mr-5 mb-5 md:mb-0" style={{ minWidth: '300px' }}>
+    <div className="flex flex-col md:flex-row w-full max-w-4xl p-5 mt-24 mb-0  mx-auto font-sans bg-projectcard hover:bg-primary shadow-lg rounded-xl transition-transform transform hover:scale-105 overflow-x-auto" style={{ minHeight: '400px' }}>
+      <div className="flex-1 flex-shrink-0 flex flex-col mt-12 items-start  md:mr-5 mb-5 md:mb-0" style={{ minWidth: '250px' }}>
         <Link to={`/casestudy/${id}`}>
           <video
             ref={videoRef}
@@ -28,7 +28,7 @@ export default function ProjectCard({ videoSrc, techstack, title, description, i
           </video>
         </Link>
       </div>
-      <div className="flex-1 flex-shrink-0 flex flex-col items-start" style={{ minWidth: '300px' }}>
+      <div className="flex-1 flex-shrink-0 flex flex-col items-start mt-12" style={{ minWidth: '300px' }}>
         <p className='text-left text-sm font-semibold text-gray-700 mb-2 prose'>
           {techItems.map((tech, index) => (
             <span key={index} className="mr-2">
@@ -38,12 +38,12 @@ export default function ProjectCard({ videoSrc, techstack, title, description, i
           ))}
         </p>
         <Link to={`/casestudy/${id}`}>
-          <h1 className='text-4xl md:text-3xl lg:text-9xl font-bold text-left hover:ml-4 transition-all duration-500 ease-in-out transform hover:scale-110'>{title}</h1>
+          <h1 className='text-4xl md:text-3xl lg:text-8xl font-bold text-left hover:ml-4 transition-all duration-500 ease-in-out transform hover:scale-110'>{title}</h1>
         </Link>
         <p className="text-gray-700 mt-4 text-left">{description}</p>
         <div className="flex space-x-4 mt-8">
-          <Link to={`https://github.com/sam-abraha/${github}`} className="border-solid bg-transparent text-black text-lg py-4 px-8 rounded hover:bg-gray-200 shadow-md transition duration-300 ease-in-out" target="_blank" rel="noopener noreferrer">Github</Link>
-          <Link to={`/casestudy/${id}`} className="border-solid bg-transparent text-black text-lg py-4 px-8 rounded hover:bg-gray-200 shadow-md transition duration-300 ease-in-out">Case Study</Link>
+          <Link to={`https://github.com/sam-abraha/${github}`} className="border-solid bg-white text-black text-lg py-4 px-8 rounded-lg hover:bg-gray-200 shadow-md transition duration-300 ease-in-out" target="_blank" rel="noopener noreferrer">Github</Link>
+          <Link to={`/casestudy/${id}`} className="border-solid bg-white text-black text-lg py-4 px-8 rounded-lg hover:bg-gray-200 shadow-md transition duration-300 ease-in-out">Case Study</Link>
         </div>
       </div>
     </div>
