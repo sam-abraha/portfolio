c7e35b7472a35725196ea88562ef27a6d0afc80b
sam <samuelabraha@gmail.com>
2026-05-21 13:59:14 +0200

    feat: update project desc in projectt.json

--- my-portfolio/src/pages/AboutMe.jsx
+++ my-portfolio/src/pages/AboutMe.jsx
@@ -22,69 +22,69 @@ const icons = [
 
 export default function AboutMe() {
   return (
-    <div className="bg-primary min-h-full flex flex-col items-center mt-10 py-16">
-      <div className="prose lg:prose-gray mx-auto p-4 text-gray-700">
+    <div className="bg-primary min-h-full flex flex-col items-center mt-10 py-16 px-4">
+      <div className="prose lg:prose-gray mx-auto text-gray-700">
         <div className="mb-8">
           <h1 className="text-4xl font-bold mb-6 bg-gradient-to-r py-2 from-pink-500 via-yellow-500 to-red-500 bg-clip-text text-transparent">Hi again, I'm Samuel.</h1>
           <p className="text-lg leading-relaxed text-justify">
             I am a computer science student based near Frankfurt, excited about solving problems and constantly seeking new knowledge,
-            particularly in software engineering, software design and cloud. My goal is to enhance my skills and broaden my knowledge in these areas.
+            particularly in software engineering, software design, and cloud. My goal is to enhance my skills and broaden my knowledge in these areas.
             When I'm not coding, I enjoy playing basketball and playing musical instruments passionately.
           </p>
         </div>
         <div className="mb-8">
           <h1 className="text-xl font-bold mb-6 text-gray-900">Skills</h1>
           <ul className="list-disc pl-5 space-y-4">
-            <li className="text-lg flex justify-between items-center">
+            <li className="text-lg flex flex-col sm:flex-row justify-between items-center">
               <span className="font-semibold">Programming languages: Java, Type/JavaScript</span>
-              <span className="flex items-center">
+              <span className="flex items-center mt-2 sm:mt-0">
                 <FaJava className="ml-2 text-orange-700" />
                 <SiTypescript className="ml-2 text-blue-700" />
               </span>
             </li>
-            <li className="flex justify-between items-center text-lg">
+            <li className="flex flex-col sm:flex-row justify-between items-center text-lg">
               <span className="font-semibold">Frameworks: React, Express</span>
-              <span className="flex items-center">
+              <span className="flex items-center mt-2 sm:mt-0">
                 <FaReact className="ml-auto text-blue-500" />
                 <FaNode className="ml-2 text-green-500" />
                 <TailwindIcon className="ml-2" />
               </span>
             </li>
-            <li className="flex justify-between items-center text-lg">
+            <li className="flex flex-col sm:flex-row justify-between items-center text-lg">
               <span className="font-semibold">Tools: Git, Postman, Excel, PowerPoint</span>
-              <span className="flex items-center">
+              <span className="flex items-center mt-2 sm:mt-0">
                 <FaGitAlt className="ml-2 text-orange-500" />
                 <SiPostman className="ml-2 text-orange-500" />
                 <SiMicrosoftexcel className="ml-2 text-green-600" />
                 <SiMicrosoftpowerpoint className="ml-2 text-red-600" />
               </span>
             </li>
-            <li className="flex justify-between items-center text-lg">
+            <li className="flex flex-col sm:flex-row justify-between items-center text-lg">
               <span className="font-semibold">Databases: PostgreSQL</span>
-              <span className="flex items-center">
-                <SiPostgresql className="ml-2 text-blue-800" />
+              <span className="flex items-center mt-2 sm:mt-0">
+                <DiPostgresql className="ml-2 text-blue-800" />
               </span>
             </li>
           </ul>
         </div>
         <div className="relative overflow-hidden mt-6 h-20">
-            <motion.div
-              className="flex space-x-6"
-              animate={{ x: ['0%', '-100%'] }}
-              transition={{ repeat: Infinity, duration: 20, ease: 'linear' }}
-            >
-              {icons.map((icon, index) => (
-                <div key={index} className="flex-none">
-                  {icon}
-                </div>
-              ))}
-              {icons.map((icon, index) => (
-                <div key={index + icons.length} className="flex-none">
-                  {icon}
-                </div>
-              ))}
-            </motion.div>
-          </div>
+          <motion.div
+            className="flex space-x-6"
+            animate={{ x: ['0%', '-100%'] }}
+            transition={{ repeat: Infinity, duration: 20, ease: 'linear' }}
+          >
+            {icons.map((icon, index) => (
+              <div key={index} className="flex-none">
+                {icon}
+              </div>
+            ))}
+            {icons.map((icon, index) => (
+              <div key={index + icons.length} className="flex-none">
+                {icon}
+              </div>
+            ))}
+          </motion.div>
+        </div>
         <div>
           <h1 className="text-xl font-bold mb-6 text-gray-900">Education</h1>
           <ul className="list-disc pl-5 space-y-4">
